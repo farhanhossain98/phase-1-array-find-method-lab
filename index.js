53b95213577fbfb064dfecea2d00c08813626f9d
farhan hossain <farhanrufon99@gmail.com>
2023-07-14 18:49:36 -0400

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+
+ 
+
+  const superbowlWin = (record) => {
+      const outcome = record.find(element => element.result === "W")
+      return outcome? outcome.year: undefined
+    }
+    
+   
+   
+   
+   
+   
+    // function superbowlWin(record) {
+        // return record.result.find === "W"
+    // }
+    // 
+    
+
+  // const superbowlWin = (record) => {
+    // const result = record.find(record.result === "W")
+    // return result 
+// } 
+
+
+// function superbowlWin(record,result) {
+//    return record.find(result === "W")
+// }
